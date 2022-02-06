a9370175bce381d2a9e4ce4543affd02229d173c
paksi011 <99129203+paksi011@users.noreply.github.com>
2022-02-06 21:54:36 +0700

    Tugas SDA Penerapan ADT
    Program Menghitung Volume Balok
    Nama : Paksi Dwi Jayanto
    NPM : G1A021011
    INFORMATIKA A
    
    Menghitung luas jajar genjang
    penjelasan sourcecode:
    
    -#include : fungsi #include adalah sebuah prosesor pengarah yang mengatakan kepada kompiler untuk meletakkan kode dari header file iostream kedalam program. Fungsi cout memerlukan file iostream.
    -using namespace std; : using name std adalah sebuah instruksi terhadap kompiler untuk menggunakan semua fungsi yang terkait untuk kerangka yang sama, bisa berupa berkas/file , class,  dan sejenisnya yang berkaitan dengan std.
    int main() : Int main() adalah fungsi utama dari sebuah kode  bahasa C, fungsi ini memberikan nilai balik menurut type datanya , dan karena memiliki nilai balik maka diberikan perintah return. Int main() artinya main program mengembalikan nilai int secara default . int main() akan mengembalikan nilai 0, dan fungsi main() tidak memiliki bagan deklarasi lokal, hanya memiliki pernyataan eksekusi berupa fungsi output.
    -cout : fungsi cout adalah fungsi standar pada c++ untuk menampilkan output ke layar.
    -cin : fungsi cin digunakan untuk memasukan data dari standar input, biasanya keyboard lalu disimpan dalam suatu variabel.
    -return 0 : return 0 digunakan untuk memberitahu bahwa kode telah selesai di eksekusi dengan exit kode 0.
    - a dan t : adalah variabel

---
+++ paksi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,t;
+int hasil;
+cout<<" Program untuk Menghitung Luas Jajar Genjang\n ";
+cout<<" Masukkan nilai alasnya : "; cin>>a;
+cout<<" Masukkan Nilai tingginya : "; cin>>t;
+
+hasil = a * t;
+cout<<" Jadi Luas Jajar Genjangnya adalah : "; cout<<hasil;
+return 0;
+}
